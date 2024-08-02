dd8aca60766f008ba5c76832be89d7016d072580
evdakofff <evdakofff@ya.ru>
2024-08-02 19:22:09 +0300

    Add files via upload

---
+++ 2proga.py
@@ -0,0 +1,5 @@
+Homework = '12'
+Time = '1,5'
+Course_name = 'Python'
+Average_time = (1.5/12)
+print('Курс:', Course_name + ',','всего задач:', Homework + ',','затрачено часов:', Time + ',','среднее время выполнения:', Average_time)
