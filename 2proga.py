0d76dc020f2b3b91b9fd0982b17e4053e2eb2ca3
evdakofff <evdakofff@ya.ru>
2024-08-02 21:44:41 +0300

    Add files via upload

--- 2proga.py
+++ 2proga.py
@@ -1,5 +1,5 @@
-Homework = '12'
-Time = '1,5'
-Course_name = 'Python'
-Average_time = (1.5/12)
-print('Курс:', Course_name + ',','всего задач:', Homework + ',','затрачено часов:', Time + ',','среднее время выполнения:', Average_time)
+Homework = 12 # Числовой тип
+Time = 1.5 # Числовой тип
+Course_name = 'Python' # Строковый тип
+Average_time = Time / Homework
+print('Курс:', Course_name + ',','всего задач:', str(Homework) + ',','затрачено часов:', str(Time) + ',','среднее время выполнения:', str(Average_time))
